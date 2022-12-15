573b99c2d44be9b9765180fafb77cf10db5bd8f2
LeonXBB <lbarenbojm@gmail.com>
2022-12-15 22:55:08 +0200

    Environment variables in main.js
    Added support of environment variables to the main script.

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 import esMain from 'es-main';
 import http from 'http'
+import * as dotenv from "dotenv";
 
 function Main() {
+  
+  dotenv.config({debug: true, path: "server.env"});
+  
   http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("");
-  }).listen(8080);
+  }).listen(process.env.PORT);
 }
 
 if (esMain(import.meta)) {
