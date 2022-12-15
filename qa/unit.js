3196913b09dbe629cd401485f6cf24aa7a89d3eb
LeonXBB <lbarenbojm@gmail.com>
2022-12-15 18:54:01 +0200

    Tests for the server
    Some tests for launching the Node.js mock server

--- qa/unit.js
+++ qa/unit.js
@@ -1,1 +1,25 @@
-import assert from "assert";
+import assert from 'assert';
+
+import mock_server from './mocks/server.js';
+import isPortReachable from 'is-port-reachable';
+
+function testServer() {
+
+    mock_server.listen(8080);
+
+    describe("mock Node.js server", function() {
+        describe("reachability", function() {
+            it("externally (via request)", async function() {
+                let res = await isPortReachable(8080, { host: "localhost" });
+                assert.equal(res, true);
+            })
+            it("internally (via args)", function() {
+                assert(mock_server._connectionKey.split("::::")[1] === "8080");
+            })
+        })
+    })
+
+    setTimeout(function() {mock_server.close()}, 1000);
+}
+
+testServer();
