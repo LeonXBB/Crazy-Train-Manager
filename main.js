b308b2c8c82ed545451ebc1e2f2dde475e6a8fb1
LeonXBB <lbarenbojm@gmail.com>
2022-12-15 22:45:02 +0200

    update of the main check
    Updated the 'main' part check to actually work in Node.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-function Main() {
-  var http = require('http');
+import esMain from 'es-main';
+import http from 'http'
 
+function Main() {
   http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("");
   }).listen(8080);
 }
 
-if (require.main === module) {
+if (esMain(import.meta)) {
   Main();
 }
