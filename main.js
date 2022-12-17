ca020cfb57a669a04fda129227fd445cbeb57c0b
LeonXBB <lbarenbojm@gmail.com>
2022-12-18 01:20:10 +0200

    Initial db creation.
    Wrote code for the initial creation of the MongoDB

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+import { MongoClient } from 'mongodb';
+
 var http = require('http');
+
+var dbURL = "mongodb://localhost:27017/ctm_db"
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  db.close();
+});
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
